932bea98fc8d6f7deb8c6c0af67331c1d891c13a
Aleksandr Brodskiy <Al@Brodskiy.com>
2018-03-15 20:31:51 -0700

    lab06

---
+++ src/labpkg/src/saferandomwalk.cpp
@@ -0,0 +1,18 @@
+// written by Aleksandr Brodskiy
+# include <cmath>
+# include <cstdlib>
+# include <ros/ros.h>
+# include <geometry_msgs/Pose2D.h>
+# include <move_base_msgs/MoveBaseAction.h>
+# include <actionlib/client/simple_action_client.h>
+int run(int c, char **v)
+{
+  ros::init(c, v, "saferandomwalk");
+  ros::NodeHandle nh;
+   
+  return 0;
+}
+int main(int argc, char **argv)
+{
+    return run(argc, argv);
+}
